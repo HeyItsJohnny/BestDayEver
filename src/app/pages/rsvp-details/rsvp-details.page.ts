80146537daa84a95ff56c2109081b1ac8304cc40
Jonathan Laroco <jonathan.laroco@gmail.com>
2018-12-05 21:43:18 -0800

    Adding RSVP Details

--- src/app/pages/rsvp-details/rsvp-details.page.ts
+++ src/app/pages/rsvp-details/rsvp-details.page.ts
@@ -1,4 +1,7 @@
+import { Rsvp, RsvpService } from 'src/app/services/rsvp.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-rsvp-details',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RsvpDetailsPage implements OnInit {
 
-  constructor() { }
+  rsvp: Rsvp = {
+    Name: '',
+    Email: '',
+    PhoneNo: '',
+    Address1: '',
+    Address2: '',
+    AddressCity: '',
+    AddressState: '',
+    AddressPostCode: '',
+    DinnerChoice: 0,
+    UpdatedAt: 0,
+    CreatedAt: 0
+  };
+
+  rsvpId = null;
+
+  constructor(private route: ActivatedRoute, private nav: NavController, private rsvpService: RsvpService, private loadingController: LoadingController) { }
 
   ngOnInit() {
+    this.rsvpId = this.route.snapshot.params['id'];
+    if (this.rsvpId)  {
+      this.loadRsvp();
+    }
+  }
+
+  async loadRsvp() {
+    const loading = await this.loadingController.create({
+      message: 'Loading RSVPs..'
+    });
+    await loading.present();
+ 
+    this.rsvpService.getRsvp(this.rsvpId).subscribe(res => {
+      loading.dismiss();
+      this.rsvp = res;
+    });
+  }
+
+  async saveRsvp() {
+ 
+    const loading = await this.loadingController.create({
+      message: 'Saving RSVPs..'
+    });
+    await loading.present();
+ 
+    if (this.rsvpId) {
+      this.rsvpService.updateRsvp(this.rsvp, this.rsvpId).then(() => {
+        loading.dismiss();
+        this.nav.goBack(true);
+      });
+    } else {
+      this.rsvpService.addRsvp(this.rsvp).then(() => {
+        loading.dismiss();
+        this.nav.goBack(true);
+      });
+    }
   }
 
 }
