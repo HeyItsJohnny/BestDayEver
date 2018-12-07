badd764b6dbea2211db5cb1f106bfe594c057f01
Jonathan Laroco <jonathan.laroco@gmail.com>
2018-12-06 16:03:38 -0800

    Edits

--- src/app/services/wedding-party.service.ts
+++ src/app/services/wedding-party.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Optional } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface WeddingParty {
+  id?: string;
+  Name: string;
+  Email: string;
+  PhoneNo: string;
+  WeddingSide: string;
+}
 
 @Injectable({
   providedIn: 'root'
