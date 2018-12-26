c0a6642d5876b9890863d6d5372b3dff916fa29d
HeyItsJohnny <HeyItsJohnny@github.com>
2018-12-25 17:37:18 -0800

    New User Service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Time } from '@angular/common';
+
+export interface User {
+  id?: string;
+  Email: string;
+  Name: string;
+  DinnerDetailsID: string;
+}
 
 @Injectable({
   providedIn: 'root'
