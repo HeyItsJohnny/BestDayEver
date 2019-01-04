41ebd980ff06cc3624a76835f07f5d35fd14067e
Jonathan Laroco <jonathan.laroco@gmail.com>
2019-01-04 12:03:21 -0800

    Adding Hella Shit.

--- src/app/members/pages/guest-details/guest-details.page.ts
+++ src/app/members/pages/guest-details/guest-details.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RsvpGuest, RsvpGuestService } from 'src/app/services/rsvp-guest.service';
 import { ActivatedRoute } from '@angular/router';
-import { NavController, LoadingController } from '@ionic/angular';
+import { NavController, LoadingController, AlertController} from '@ionic/angular';
 import { Router } from '@angular/router';
+import { Dinner, DinnerService } from 'src/app/services/dinner.service';
 
 @Component({
   selector: 'app-guest-details',
@@ -10,6 +11,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./guest-details.page.scss'],
 })
 export class GuestDetailsPage implements OnInit {
+
+  dinners: Dinner[];
 
   rsvpGuest: RsvpGuest = {
     Name: '',
@@ -25,13 +28,18 @@ export class GuestDetailsPage implements OnInit {
     private nav: NavController, 
     private rsvpGuestService: RsvpGuestService, 
     private loadingController: LoadingController,
-    private router: Router) { }
+    private router: Router,
+    private alertController: AlertController,
+    private dinnerService: DinnerService) { }
 
   ngOnInit() {
     this.rsvpGuestID = this.route.snapshot.params['id'];
     if (this.rsvpGuestID)  {
       this.loadRsvpGuest();
     }
+    this.dinnerService.getDinners().subscribe(res => {
+      this.dinners = res;
+    });
   }
 
   async loadRsvpGuest() {   
@@ -66,14 +74,45 @@ export class GuestDetailsPage implements OnInit {
   }
 
   async goToDinnerSelection() {
+    this.saveRsvp();
+    var options = {
+      header: "Dinner Selection",
+      subHeader: "Please select a dinner",
+      inputs: [],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data: any) => {
+            console.log('Radio data:', data);
+          }
+        }
+      ]
+    };
+
+    for (let item of this.dinners) {
+      options.inputs.push({ name : item.Name, value: item.id , label: item.Name, type: 'radio'});
+    }
+    
+    let alert = await this.alertController.create(options);
+    await alert.present();
+  }
+
+  saveRsvp() {
     if (this.rsvpGuestID ) {
       this.rsvpGuestService.updateRsvpGuest(this.rsvpGuest, this.rsvpGuestID).then(docRef => {
-        this.router.navigateByUrl('/members/dinnerSelectionList/' + this.rsvpGuestID);
+        //this.router.navigateByUrl('/members/dinnerSelectionList/' + this.rsvpGuestID);
       });
     } else {
       this.rsvpGuestService.addRsvpGuest(this.rsvpGuest).then(docRef => {
         this.rsvpGuestID = docRef.id;
-        this.router.navigateByUrl('/members/dinnerSelectionList/' + this.rsvpGuestID);
+        //this.router.navigateByUrl('/members/dinnerSelectionList/' + this.rsvpGuestID);
       });
     }
   }
