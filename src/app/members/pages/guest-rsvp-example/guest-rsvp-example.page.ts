5f1d739c065fbdede6999118d8d02e3c487fb3fb
Jonathan Laroco <jonathan.laroco@gmail.com>
2019-01-07 10:04:18 -0800

    Cha Cha Changes.

--- src/app/members/pages/guest-rsvp-example/guest-rsvp-example.page.ts
+++ src/app/members/pages/guest-rsvp-example/guest-rsvp-example.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Rsvp, RsvpService } from 'src/app/services/rsvp.service';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFireAuth } from "angularfire2/auth";
+import { Profile } from 'src/app/services/profile.service';
 import { RsvpGuest, RsvpGuestService } from 'src/app/services/rsvp-guest.service';
 
 @Component({
@@ -8,22 +11,40 @@ import { RsvpGuest, RsvpGuestService } from 'src/app/services/rsvp-guest.service
   styleUrls: ['./guest-rsvp-example.page.scss'],
 })
 export class GuestRsvpExamplePage implements OnInit {
+  private rsvpsCollection: AngularFirestoreCollection<Rsvp>;
 
-  constructor() { }
+  constructor(
+    db: AngularFirestore,
+    private afAuth: AngularFireAuth) { 
+      var authUser = this.afAuth.auth.currentUser;
+      //var test = db.collection<Profile>('profile').doc(authUser.uid).collection('weddingguests');
+  
+      this.rsvpsCollection  = db.collection<Profile>('profile').doc(authUser.uid).collection('weddingguests');
+    }
 
-  findRsvpGuest: RsvpGuest = {
+  findRsvp: Rsvp = {
     Name: '',
     Email: '',
     PhoneNo: '',
-    DinnerChoice: '',
-    DinnerChoiceText: ''
+    Address1: '',
+    Address2: '',
+    AddressCity: '',
+    AddressState: '',
+    AddressPostCode: '',
+    UpdatedAt: 0,
+    isGoing: false,
+    NumberOfGuests: 0,
+    CreatedAt: 0
   };
 
   ngOnInit() {
   }
 
-  findGuestRSVP() {
-    console.log("RSVP GUEST NAME: " + this.findRsvpGuest.Name);
+  findRSVP() {
+    console.log("RSVP GUEST NAME: " + this.findRsvp.Name);
+    var query = this.rsvpsCollection.ref.where("Name","==",this.findRsvp.Name).get().then(docRef => {
+      console.log("Query: " + docRef);
+    });
   }
 
 }
