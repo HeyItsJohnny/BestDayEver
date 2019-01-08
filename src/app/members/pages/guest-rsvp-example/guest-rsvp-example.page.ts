368649b02bc3a2fb69c76d0a4900c0d8c4dee707
jonathan laroco <jonathanlaroco@jonathans-MacBook-Pro.local>
2019-01-07 22:09:57 -0800

    Fucking around with shit..

--- src/app/members/pages/guest-rsvp-example/guest-rsvp-example.page.ts
+++ src/app/members/pages/guest-rsvp-example/guest-rsvp-example.page.ts
@@ -3,7 +3,8 @@ import { Rsvp, RsvpService } from 'src/app/services/rsvp.service';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { AngularFireAuth } from "angularfire2/auth";
 import { Profile } from 'src/app/services/profile.service';
-import { RsvpGuest, RsvpGuestService } from 'src/app/services/rsvp-guest.service';
+import { NavController, LoadingController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-guest-rsvp-example',
@@ -15,7 +16,10 @@ export class GuestRsvpExamplePage implements OnInit {
 
   constructor(
     db: AngularFirestore,
-    private afAuth: AngularFireAuth) { 
+    private afAuth: AngularFireAuth,
+    private loadingController: LoadingController,
+    public alertController: AlertController,
+    private rsvpService: RsvpService) { 
       var authUser = this.afAuth.auth.currentUser;  
       this.rsvpsCollection  = db.collection<Profile>('profile').doc(authUser.uid).collection('weddingguests');
     }
@@ -35,25 +39,53 @@ export class GuestRsvpExamplePage implements OnInit {
     CreatedAt: 0
   };
 
+  getRsvp: Rsvp = {
+    Name: '',
+    Email: '',
+    PhoneNo: '',
+    Address1: '',
+    Address2: '',
+    AddressCity: '',
+    AddressState: '',
+    AddressPostCode: '',
+    UpdatedAt: 0,
+    isGoing: false,
+    NumberOfGuests: 0,
+    CreatedAt: 0
+  };
+
   ngOnInit() {
   }
 
   findRSVP() {
-    console.log("RSVP GUEST NAME: " + this.findRsvp.Name);
+    this.getRSVP();
+    //console.log("RSVP!!! " + test);
+  }
+
+  getRSVP() {
+    console.log("RSVP NAME TO FIND:: " + this.findRsvp.Name);
     this.rsvpsCollection.ref.where("Name","==",this.findRsvp.Name)
+    .limit(1)
     .get()
     .then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
-            if (doc.id != null) {
-              console.log(doc.id, " => ", doc.data());
-            } else {
-              console.log("There is NO DATA HERE..");
-            }            
+          console.log("DOC ID: " + doc.id);
+          //console.log(doc.id, " => ", doc.data());
+          this.getRsvp = doc.data();
+          console.log("GET RSVP OBJECT!!!");
+          console.log(this.getRsvp);
+          //return doc.id;
         });
     })
     .catch(function(error) {
-        console.log("Error getting documents: ", error);
+        //console.log("RETURN NOTHING..");
     });
   }
 
+  /*async getRSVP2(DocID: string) {
+    this.rsvpService.getRsvp(DocID).subscribe(res => {
+      this.getRsvp = res;
+      console.log("FOUND RSVP NAME!!!! " + this.getRsvp.Name);
+    });
+  }*/
 }
