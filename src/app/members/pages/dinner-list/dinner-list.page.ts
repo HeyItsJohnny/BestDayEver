10b564a25ca343a7ed3578f8d1d5726a71431040
jonathan laroco <jonathanlaroco@jonathans-MacBook-Pro.local>
2019-01-15 21:48:05 -0800

    Changing Dinner Page.

--- src/app/members/pages/dinner-list/dinner-list.page.ts
+++ src/app/members/pages/dinner-list/dinner-list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dinner, DinnerService } from 'src/app/services/dinner.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-dinner-list',
@@ -10,7 +11,9 @@ export class DinnerListPage implements OnInit {
 
   dinners: Dinner[];
 
-  constructor(private dinnerService: DinnerService) { }
+  constructor(
+    private dinnerService: DinnerService,
+    public alertController: AlertController) { }
 
   ngOnInit() {
     this.dinnerService.getDinners().subscribe(res => {
@@ -19,5 +22,52 @@ export class DinnerListPage implements OnInit {
   }
   remove(item) {
     this.dinnerService.removeDinner(item.id);
+  }
+
+  addItem() {
+    console.log("ADDING DINNER..");
+  }
+
+  updateItem(item: Dinner) {
+    console.log("Updating Item: " + item.id);
+  }
+
+  displayPrompt(dinnerID: string, dinnerObj: Dinner) {
+    this.alertController.create({
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'Dinner',
+          type: 'text',
+          value: dinnerObj.Name,
+          placeholder: 'Dinner'
+        },
+        {
+          name: 'Description',
+          type: 'text',
+          value: dinnerObj.Description,
+          placeholder: 'Description'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            if (dinnerID == "") {
+              this.dinnerService.addDinner(dinnerObj);
+            } else {
+              this.dinnerService.updateDinner(dinnerObj, dinnerID);
+            }
+          }
+        }
+      ]
+    }).then(alert => alert.present());
   }
 }
