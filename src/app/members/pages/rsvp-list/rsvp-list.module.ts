5f3fe08c13d0e36d994b9d06ef2de7c9a41ae1cd
Jonathan Laroco <jonathan.laroco@gmail.com>
2019-01-22 11:36:04 -0800

    test

--- src/app/members/pages/rsvp-list/rsvp-list.module.ts
+++ src/app/members/pages/rsvp-list/rsvp-list.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { RsvpListPage } from './rsvp-list.page';
