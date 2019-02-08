494e3b63b790a2dd87d9252e288716ec4660db25
Jonathan Laroco <jonathan.laroco@gmail.com>
2019-02-08 09:57:47 -0800

    Adding budget

--- src/app/members/pages/budget-details/budget-details.page.ts
+++ src/app/members/pages/budget-details/budget-details.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavController, LoadingController, AlertController } from '@ionic/angular';
+import { Budget, BudgetService } from 'src/app/services/budget.service';
+
 
 @Component({
   selector: 'app-budget-details',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-details.page.scss'],
 })
 export class BudgetDetailsPage implements OnInit {
+  
+  budget: Budget = {
+    Category: '',
+    SubCategory: '',
+    BudgetName: '',
+    SearchName: '',
+    EstimatedCost: 0,
+    ActualCost: 0,
+    Deposit: 0,
+    Comments: ''
+  };
 
-  constructor() { }
+  budgetId = null;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private nav: NavController, 
+    private budgetService: BudgetService, 
+    public alertController: AlertController,
+    private loadingController: LoadingController) { }
 
   ngOnInit() {
+    this.budgetId = this.route.snapshot.params['id'];
+    if (this.budgetId)  {
+      this.loadBudget();
+    }
   }
+
+  async loadBudget() {   
+    const loading = await this.loadingController.create({
+      message: 'Loading Dinner..'
+    });
+    await loading.present();
+ 
+    this.budgetService.getBudget(this.budgetId).subscribe(res => {
+      loading.dismiss();
+      //this.budget = res;
+    });
+  }
+
 
 }
